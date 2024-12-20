ed83eca7269666a288670fb02a8d2f7f02e08405
LucaDoro <luca.doro31@gmail.com>
2024-12-20 16:49:05 +0100

    Fuck this

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const menuList = document.querySelector(".menu-list");
 const menuToggle = document.querySelector(".burger-menu");
+const menuList = document.querySelector(".menu-list");
 
 menuToggle.addEventListener("click", () => {
   menuList.classList.toggle("active");
